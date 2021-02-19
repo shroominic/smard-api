90409d441f532dc6595f4551a87fd2088a3273fb
Dominic <db2002dominic@gmail.com>
2021-02-19 06:55:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Smard API
+
+A simple script to get data from [Smard.de](https://www.smard.de/home/downloadcenter/download-marktdaten)
+
+
+
+
---
+++ smard_api/smard_api.py
@@ -0,0 +1,42 @@
+import requests, json, time
+import pandas as pd
+from io import StringIO
+
+# get unix timestamp in milliseconds    
+timestamp = int(time.time()) * 1000
+
+# request smard data with default values
+def requestSmardData(
+    modulIDs = [8004169], 
+    timestamp_from_in_milliseconds = (int(time.time()) * 1000) - (4*3600)*1000, 
+    timestamp_to_in_milliseconds   = (int(time.time()) * 1000),
+    region   = "DE",
+    language = "de",
+    type     = "discrete"
+    ):
+    
+    # http request content
+    url  = "https://www.smard.de/nip-download-manager/nip/download/market-data"
+    body = json.dumps({
+        "request_form": [
+            {
+                "format": "CSV",
+                "moduleIds": modulIDs,
+                "region": region,
+                "timestamp_from": timestamp_from_in_milliseconds,
+                "timestamp_to": timestamp_to_in_milliseconds,
+                "type": type,
+                "language": language
+            }]})
+    
+    # http response
+    data = requests.post(url, body)
+
+    # create pandas dataframe out of response string (csv)
+    df = pd.read_csv(StringIO(data.text), sep=';')
+    
+    return df
+
+
+df = requestSmardData()
+print(df)
